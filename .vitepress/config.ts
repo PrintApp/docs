5ea7a8dd5fe408018a9811d03faab70d13cde02e
Isaac Coker <81635623+iocoker@users.noreply.github.com>
2023-04-11 08:25:10 +0200

    Empty pages created

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -17,12 +17,65 @@ export default defineConfig({
 
         sidebar: [
             {
-                text: 'Examples',
+                text: 'Getting started',
                 items: [
-                    { text: 'Markdown Examples', link: '/markdown-examples' },
-                    { text: 'Runtime API Examples', link: '/api-examples' },
+                    { text: 'What is Print.App', link: '/what-is-printapp' },
+                    { text: 'Installation', link: '/installation/index' },
                 ],
             },
+            {
+                text: 'Guides',
+                items: [
+                    { text: 'How to create a design', link: '/guides/create-design' },
+                    { text: 'Smart Texts', link: '/guides/smart-texts' },
+                    { text: 'Display modes', link: '/guides/display-modes' },
+                    { text: 'Colors', link: '/guides/colors' },
+                    { text: 'Fonts', link: '/guides/fonts' },
+                    { text: 'Graphics', link: '/guides/graphics' },
+                ]
+            },
+            {
+                text: 'Solutions',
+                items: [
+                    { text: 'Stationeries', link: '/solutions/stationeries' },
+                    { text: 'Photo Prints', link: '/solutions/photo-prints' },
+                    { text: 'Garment prints', link: '/solutions/garment-prints' },
+                    { text: 'Large banner prints', link: '/solutions/large-banner-prints' },
+                    { text: 'Process printing', link: '/solutions/process-printing' },
+                    { text: 'DTF, DTG, Sublimation', link: '/solutions/dtf-dtg-sublimation' },
+                    { text: 'Calendars', link: '/solutions/calendars' },
+                ]
+            },
+            {
+                text: 'Customization',
+                items: [
+                    { text: 'Webhooks', link: '/customization/webhooks' },
+                    { text: 'Configuration', link: '/customization/configuration' },
+                ]
+            },
+            {
+                text: 'Installation',
+                items: [
+                    { text: 'Wordpress', link: '/installation/wordpress' },
+                    { text: 'Opencart', link: '/installation/opencart' },
+                    { text: 'Prestashop', link: '/installation/prestashop' },
+                    { text: 'Shopify', link: '/installation/shopify' },
+                    { text: 'Etsy', link: '/installation/etsy' },
+                    { text: 'Squarespace', link: '/installation/squarespace' },
+                    { text: 'BigCommerce', link: '/installation/bigcommerce' },
+                    { text: 'Wix', link: '/installation/wix' },
+                    { text: 'Custom installation', link: '/installation/custom-installation' },
+                ]
+            },
+            {
+                text: 'Modules',
+                items: [
+                    { text: 'Calendars', link: '/modules/calendars' },
+                    { text: 'Canvas Adjuster', link: '/modules/canvas-adjuster' },
+                    { text: 'Product Colors', link: '/modules/product-colors' },
+                    { text: 'Variable Data', link: '/modules/variable-data' },
+                ]
+            }
         ],
 
         socialLinks: [
